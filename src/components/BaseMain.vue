26eb21467067e8baa64c91c60ba0cef6eb48b541
MrGians <gianlumura97@gmail.com>
2022-07-28 17:10:10 +0200

    movie language it & en transformed to img

--- src/components/BaseMain.vue
+++ src/components/BaseMain.vue
@@ -4,7 +4,15 @@
       <li v-for="movie in movies" :key="movie.id">
         Titolo: {{ movie.title }}, <br />
         Titolo Originale: {{ movie.original_title }}, <br />
-        Lingua: {{ movie.original_language }}, <br />
+        <div v-if="movie.original_language === 'it' || movie.original_language === 'en'">
+          Lingua:
+          <img
+            width="20"
+            :src="require(`./../assets/img/flags/${movie.original_language}.png`)"
+            alt="Language Flag"
+          />
+        </div>
+        <div v-else>Lingua: {{ movie.original_language }}, <br /></div>
         Voto: {{ movie.vote_average }} . <br />
         <hr />
       </li>
